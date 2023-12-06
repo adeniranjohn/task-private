b1ba9c71086363a6477f37dd7e6f164a4321c1d8
John Adeniran <jasolajohn@gmail.com>
2023-12-06 12:42:31 +0100

    Feat:visibilty-true

--- app/Controllers/Http/GithubController.ts
+++ app/Controllers/Http/GithubController.ts
@@ -4,25 +4,29 @@ import axios from 'axios'
 
 export default class GithubController {
 
-  public async index({ request, response }: HttpContextContract) {
+  public async index({ request }: HttpContextContract) {
     const GITHUB_API: string = Env.get(`GITHUB_API`)
     const GITHUB_USER: string = Env.get(`GITHUB_USER`)
     const GITHUB_TOKEN: string = Env.get(`GITHUB_TOKEN`)
 
-    const { limit = 10, username = 'adeniranjohn', page = 0 } = request.qs()
+    const { limit = 10, username = 'adeniranjohn', page = 0, visibility = 'private' } = request.qs()
 
-    const data = await axios.get(`${GITHUB_API}?q=user:${username}&page=${page}`, {
-      responseType: 'json',
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-      params: {
-        per_page: limit,
-      },
-    })
+    const data = await axios.get(
+      `${GITHUB_API}?q=user:${username}&page=${page}&visibility=${visibility}`,
+      {
+        responseType: 'json',
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+        params: {
+          per_page: limit,
+        },
+      }
+    )
 
-    return data.data.items.map(
-      ({ name, full_name, url, html_url, created_at, updated_at, visibility }) => ({
+    return data.data.items
+      .filter((item) => item.visibility === visibility)
+      .map(({ name, full_name, url, html_url, created_at, updated_at, visibility }) => ({
         name,
         full_name,
         url,
@@ -30,7 +34,6 @@ export default class GithubController {
         created_at,
         updated_at,
         visibility,
-      })
-    )
+      }))
   }
 }
